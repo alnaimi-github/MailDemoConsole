b1d9496a675273ff72b6154360c7ac2f8f731a7d
alnaimi-github <alnymybdalwas1@gmail.com>
2025-04-14 20:32:26 +0300

    Add project files.

---
+++ MailDemoConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+
+var message = new MimeMessage();
+
+var from = new MailboxAddress("alnuaimi","abdulwaisaalnuaimi@gmail.com");
+message.From.Add(from);
+
+
+var to = new MailboxAddress("waisa", "alnymybdalwas1@gmail.com");
+message.To.Add(to);
+
+message.Subject = "Hi waisa";
+
+var bb = new BodyBuilder();
+bb.TextBody = "Hello Waisa";
+bb.HtmlBody = "<p> How are you doing? \r\n I hope you are doing well. \r\n Best regards, \r\n Abdulwais Alnuaimi</p>";
+
+message.Body = bb.ToMessageBody();
+
+
+using var smtp = new SmtpClient();
+await smtp.ConnectAsync("localhost", 11230);
+await smtp.SendAsync(message);
+await smtp.DisconnectAsync(true);
+
+Console.WriteLine($"Mail send to {to.Address}");
