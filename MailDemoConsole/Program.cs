205b448e14df259e3fa0249ef0469e94f0aa341b
alnaimi-github <alnymybdalwas1@gmail.com>
2025-04-14 20:51:52 +0300

    Embed image in email body and update HTML structure
    - Include `MimeKit.Utils` namespace.
    - Comment out attachment of `test.png`.
    - Add image as a linked resource using `bb.LinkedResources.Add(imagePath)`.
    - Generate `ContentId` for the image with `MimeUtils.GenerateMessageId()`.
    - Update `HtmlBody` to embed image using `ContentId`.
    - Enhance `HtmlBody` with a greeting, image note, and closing.

--- MailDemoConsole/Program.cs
+++ MailDemoConsole/Program.cs
@@ -2,6 +2,7 @@
 using MailKit.Net.Smtp;
 using MimeKit;
 using MimeKit.Text;
+using MimeKit.Utils;
 
 var message = new MimeMessage();
 
@@ -16,10 +17,19 @@ message.Subject = "Hi waisa";
 
 var bb = new BodyBuilder();
 
-bb.Attachments.Add("H:\\Development\\My Learning\\Email projects\\MailDemoConsole\\MailDemoConsole\\test.png");
+//bb.Attachments.Add("H:\\Development\\My Learning\\Email projects\\MailDemoConsole\\MailDemoConsole\\test.png");
+
+var imagePath = "H:\\\\Development\\\\My Learning\\\\Email projects\\\\MailDemoConsole\\\\MailDemoConsole\\\\test.png";
+var imageEntity = bb.LinkedResources.Add(imagePath);
+imageEntity.ContentId = MimeUtils.GenerateMessageId();
+
 
 bb.TextBody = "Hello Waisa";
-bb.HtmlBody = "<p> How are you doing? \r\n I hope you are doing well. \r\n Best regards, \r\n Abdulwais Alnuaimi</p>";
+bb.HtmlBody = "<p>Hello Waisa,</p>" 
+              +
+              "<p>Hope you are doing well. Please find the attached image below:</p><img src=\"cid:" 
+              + imageEntity.ContentId + "\"><p>Best regards,<br>Alnuaimi</br></p>";
+
 
 message.Body = bb.ToMessageBody();
 
